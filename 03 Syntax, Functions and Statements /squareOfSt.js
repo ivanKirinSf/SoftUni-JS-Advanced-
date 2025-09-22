7adf64fd920ef3a2d78bdcc8a6490a5189f0fefe
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-09-22 19:47:18 +0300

    Create squareOfSt.js

---
+++ 03 Syntax, Functions and Statements /squareOfSt.js
@@ -0,0 +1,16 @@
+function squareOfSt(input){
+
+    let num = Number(input);
+
+    let str = "";
+
+    for(let i = 0; i<num; i++){
+        
+        console.log('* '.repeat(num));
+    }
+
+    //console.log(str)
+
+}
+
+squareOfSt(10)
