a29b954e626f425225961e2340a7b9defcbb64ec
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-10-04 19:13:24 +0300

    Update 04 previousDay.js

--- 04 Syntax, Functions and Statements - Exercise/04 previousDay.js
+++ 04 Syntax, Functions and Statements - Exercise/04 previousDay.js
@@ -1,15 +1,7 @@
 function previousDay(year, month, day){
 
-    let info = new Date(year, month, day);
+let info = new Date(year, month-1, day-1);
 
-    let newInfo = info.getDate();
-
-    newInfo = newInfo - 1;
-
-    info.set(newInfo[day])
-
-    console.log(info)
-
-    //console.log
+console.log(`${info.getFullYear()}-${info.getMonth()+1}-${info.getDate()}`);
 
 }
