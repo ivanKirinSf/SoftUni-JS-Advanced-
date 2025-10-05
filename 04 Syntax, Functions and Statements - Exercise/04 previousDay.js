962178ed28fe8be9b9913b951bde58b01df953bb
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-10-05 12:12:03 +0300

    Update 04 previousDay.js

--- 04 Syntax, Functions and Statements - Exercise/04 previousDay.js
+++ 04 Syntax, Functions and Statements - Exercise/04 previousDay.js
@@ -2,8 +2,8 @@ function previousDay(year, month, day){
 
 let currentDate = new Date(year, month-1, day);
 
-currentDate.setDate(currentDate.getDate()-1);
+currentDate.setDate(currentDate.getDate() - 1);
 
-console.log(`${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`)
+console.log(`${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`);
 
 }
