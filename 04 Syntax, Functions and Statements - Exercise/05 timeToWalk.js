ade1176337dc7850764d39b1e87ee3c8279f6d2a
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-10-09 20:43:49 +0300

    Update 05 timeToWalk.js

--- 04 Syntax, Functions and Statements - Exercise/05 timeToWalk.js
+++ 04 Syntax, Functions and Statements - Exercise/05 timeToWalk.js
@@ -8,13 +8,13 @@ function getTimeToUniversity(steps, footprintLength, speedKmH) {
 
     //console.log(breaks)
 
-    let timeMSec = new Date ((distMeters/speedInMperSec + breaks)*1000);
+    let timeMSec = new Date (Math.round(distMeters/speedInMperSec + breaks)*1000);
 
     let finalTime = timeMSec.toISOString();
 
-    let time = finalTime.substring(10, 8);
+    let time = finalTime.substring(11, 19);
 
-    console.log(finalTime)
+    //console.log(finalTime)
 
     console.log(time)
 
