78556cda9dc616f5f0f8a09471ee5282ea7fbb75
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-10-17 20:02:48 +0300

    Update 07 cookingByNum.js

--- 04 Syntax, Functions and Statements - Exercise/07 cookingByNum.js
+++ 04 Syntax, Functions and Statements - Exercise/07 cookingByNum.js
@@ -1,23 +1,26 @@
 function cookingByNum(arg, ...params){
 
- let programs = {
 
-    "chop" : (num) => num/2,
-    "dice" : (num) => Math.sqrt(num),
-    "spice" : (num) => num +1,
-    "bake": (num) => num*3,
-    "fillet" : (num) => num*0.8,
+   let operations = {
 
- }
+      "chop" : (num) => num/2,
+      "dice" : (num) => Math.sqrt(num),
+      "spice" : (num) => num +1,
+      "bake" : (num) => num*3,
+      "fillet" : (num) => num*0.8,
 
- let num = Number(arg);
+   }
 
- for(let op of params){
+   let num = Number(arg);
 
-    num = programs[op](num);
+   for(let el of params){
 
-    console.log(num)
+      num = operations[el](num);
 
- }
+      console.log(num)
+
+   }
 
 }
+
+cookingByNum('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
