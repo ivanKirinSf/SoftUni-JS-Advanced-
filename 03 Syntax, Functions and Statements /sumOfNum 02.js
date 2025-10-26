8e56ad5231f38eabc7437c0d7bcaa490dcc83435
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-10-26 14:01:38 +0200

    Create sumOfNum 02.js

---
+++ 03 Syntax, Functions and Statements /sumOfNum 02.js
@@ -0,0 +1,19 @@
+function sumOfNum(a, b){
+
+    let num1 = Number(a);
+
+    let num2 = Number(b);
+
+    let sum = 0;
+
+    for(let i = num1; i <= num2; i++){
+
+        sum += i
+
+    }
+
+    console.log(sum)
+
+}
+
+sumOfNum('-8', '20')
