2117437970c36839bcbebe6af7f3eb92dfb1c372
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-11-09 00:09:46 +0200

    Update 05 timeToWalk 02.js

--- 04 Syntax, Functions and Statements - Exercise/05 timeToWalk 02.js
+++ 04 Syntax, Functions and Statements - Exercise/05 timeToWalk 02.js
@@ -1,16 +1,48 @@
 function timeToWalk(steps, footprint, speed){
 
-   let lengthInKM = steps*(footprint/1000);
+    let time = new Date ()
 
-   let speedInMlSec = (speed/3600)*1000;
+   let footprintKM = footprint/1000;
+
+   let lengthInKM = steps*footprintKM;
+
+   let restNum = Math.floor(lengthInKM/0.5);
+
+   //console.log(restNum)
+
+   
 
    //let speedIn
 
-   let timeInMlSec = speedInMlSec*lengthInKM;
+   let timeInSec = ((lengthInKM/speed)*3600)+4*60;
+
+   if(timeInSec / 3600 < 1){
+
+    time.setHours(00)
+
+   }else{
+
+    let hoursNum = timeInSec / 3600;
+
+    time.setHours(hoursNum)
+
+   }
+
+   let min = Math.floor(timeInSec/60);
+
+   time.setMinutes(min);
+
+   //console.log(min)
+
+   let sec = timeInSec; 
+   
+   console.log(sec)
+
+   //time.setSeconds(sec);
 
 
    
-   console.log(timeInMlSec)
+   console.log(time)
 
 }
 
