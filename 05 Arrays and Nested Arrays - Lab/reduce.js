21990a40e81f97f4c61163c33eca46477a1de735
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-12-06 22:17:47 +0200

    Create reduce.js

---
+++ 05 Arrays and Nested Arrays - Lab/reduce.js
@@ -0,0 +1,18 @@
+let myArr = [30, 50, 40, 10, 70];
+
+let result = 0;
+
+let reduced = myArr.reduce(total, element, indexedDB, arr) => {
+
+    
+    if(index <= myArr.length-1){
+        result = total + element
+    }else{
+
+        total
+
+    }
+
+}
+
+console.log(reduced)
