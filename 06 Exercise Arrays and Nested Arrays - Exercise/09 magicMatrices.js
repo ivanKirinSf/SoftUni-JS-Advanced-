24f8b7b5ad63e6963aec187342d0b911c4d82894
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-01-20 20:20:23 +0200

    Update 09 magicMatrices.js

--- 06 Exercise Arrays and Nested Arrays - Exercise/09 magicMatrices.js
+++ 06 Exercise Arrays and Nested Arrays - Exercise/09 magicMatrices.js
@@ -1,29 +1,40 @@
-function magicMat(arr){
+function magicMatrix(arr){
+
+for(let i = 0; i < arr.length-1; i++){
 
     let rowOne = 0;
     let rowTwo = 0;
     let colOne = 0;
     let colTwo = 0;
 
-    
+    arr[i].forEach(element =>  rowOne += Number(element));
+    arr[i+1].forEach(element =>  rowTwo += Number(element));
+    arr.forEach(row =>  colOne += Number(row[i]));
+    arr.forEach(row =>  colTwo += Number(row[i+1]));
 
-   for(let i = 0; i < arr.length; i++){
+    //console.log(colTwo)
 
-       arr[i].forEach(el => rowOne += el);
-       arr[i+1].forEach(el => rowTwo += el);
-       arr.forEach(row=> colOne += row[i]);
-       arr.forEach(row=> colTwo += (row[i]));
-
-       if(rowOne !== rowTwo || colOne !== colTwo){
+    if(rowOne !== rowTwo || colOne !== colTwo){
 
        return false;
 
-   }else{
+    }else{
 
-       return true
+        return true;
+    }
 
-   }   
+}
 
-   }        
-    } 
+//console.log(rowOne)
+
+}
+
+
+
+console.log(magicMatrix(
+[[1, 0, 0],
+ [0, 0, 1],
+ [0, 1, 0]]
+))
+
 
