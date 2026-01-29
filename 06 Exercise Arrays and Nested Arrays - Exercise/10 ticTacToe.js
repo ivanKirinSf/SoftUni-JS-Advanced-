4fb25329e3de6a57adedb30b29ba02d5264c31e9
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-01-29 21:26:27 +0200

    Update 10 ticTacToe.js

--- 06 Exercise Arrays and Nested Arrays - Exercise/10 ticTacToe.js
+++ 06 Exercise Arrays and Nested Arrays - Exercise/10 ticTacToe.js
@@ -1,102 +1,109 @@
 function ticTacToe(data) {
 
-    let initialDashboard = [
+let initialDashboard = [
 
-        [false, false, false],
-        [false, false, false],
-        [false, false, false]
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
 
-    ];
+];
 
-    let isPlayerOnePlay = true;
+let isPlayerOnePlay = true;
 
-    for (let coordinates of data) {
 
-        let [x, y] = coordinates.split(" ");
+for(let coordinates of data){
 
-        let marker = isPlayerOnePlay ? "X" : "O";
+    let [x, y] = coordinates.split(" ");
 
-        if (initialDashboard[x][y]) {
+    let marker = isPlayerOnePlay ? "X" : "O";
 
-            console.log("This place is already taken. Please choose another!");
+    if(initialDashboard[x][y]){
 
-            continue;
-        }
+        console.log("This place is already taken. Please choose another!");
+        continue;
 
-        initialDashboard[x][y] = marker;
+    }
 
-        if (checkWin(initialDashboard, marker)) {
+    initialDashboard[x][y] = marker;
 
-            console.log(`Player ${marker} wins!`);
+    if(checkWin(initialDashboard, marker)){
 
-        }
+        console.log(`Player ${marker} wins!`);
 
-        if (!checkFreeSpace(initialDashboard)) {
+        return;
 
-            console.log("The game ended! Nobody wins :(")
+    }
 
-            return
 
-        }
 
-        isPlayerOnePlay = !isPlayerOnePlay;
+    if(!checkFreeSpace(initialDashboard)){
 
-        function checkWin(dashboard, market) {
+        console.log("The game ended! Nobody wins :(") 
 
-            for (let i = 0; i < dashboard.length; i++) {
-                if (dashboard[i][0] === marker &&
-                    dashboard[i][1] === marker &&
-                    dashboard[i][2] === marker) {
+        return;        
 
-                    return true;
+    }
 
-                }
+    isPlayerOnePlay = !isPlayerOnePlay;
 
-                else if (
+}
+
+
+
+function checkWin(dashboard, marker){
+
+ for(let i = 0; i< dashboard.length; i++){
+            if(dashboard[i][0] === marker &&
+                dashboard[i][1] === marker &&
+                dashboard[i][2] === marker){
+
+                  return true;
+
+
+                }else if(
+
                     dashboard[0][i] === marker &&
                     dashboard[1][i] === marker &&
-                    dashboard[2][i] === marker) {
+                    dashboard[2][i] === marker
 
-                    return true;
+                ){
 
-                } else if (
+                   return true;
 
+                }else if(
                     dashboard[0][0] === marker &&
                     dashboard[1][1] === marker &&
                     dashboard[2][2] === marker
-
-                ) {
+                ){
 
                     return true;
-                } else if (
+
+                } else if(
 
                     dashboard[0][2] === marker &&
                     dashboard[1][1] === marker &&
-                    dashboard[2][0] === marker
-                ) {
+                    dashboard[2][0] === marker  
+                ){
 
-                    return true;
+                    return true; 
                 }
 
-                return false
+                return false; 
 
-
-            }
         }
 
-        function checkFreeSpace(dashboard) {
-
-            return !!dashboard.flat().filter(x => !x).length
-        }
+};
 
 
-    }
 
-    function printDashboard(dashboard) {
+function checkFreeSpace(dashboard){
 
-        dashboard.forEach(row => console.log(row.join("/t")))
+    return !!dashboard.flat().filter(x => !x).length;
 
-    }
+}; 
+
 }
+
+
 
 
