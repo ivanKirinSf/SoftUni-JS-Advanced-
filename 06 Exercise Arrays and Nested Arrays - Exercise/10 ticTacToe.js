573dfbcb6428546ac15f0a64065229517dc7a70d
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-01-31 14:38:25 +0200

    Update 10 ticTacToe.js

--- 06 Exercise Arrays and Nested Arrays - Exercise/10 ticTacToe.js
+++ 06 Exercise Arrays and Nested Arrays - Exercise/10 ticTacToe.js
@@ -5,50 +5,132 @@ function ticTacToe(arr){
    let coordinates = [];
 
  let startDashboard = [
+
                        [false, false, false],
                        [false, false, false],
                        [false, false, false],
+
                      ]
 
-                     let index = 0
-                     for(let el of arr){
+                     
+                     for(let k = 0; k < arr.length; k++){
 
-                        coordinates = el.split(" ");
+                        coordinates = arr[k].split(" ");
 
-
-
-
-                        if(index % 2 === 0){
+                        if(k % 2 === 0){
 
                            marker = "X";
                            
-                        }else if(index % 2 !== 0){
+                        }else if(k % 2 !== 0){
 
                            marker = "O"
                         }
 
-                        if(isEmpty(startDashboard)){
+                         if(startDashboard[Number(coordinates[0])][Number(coordinates[1])] === false){
 
-                           console.log("This place is already taken. Please choose another!")
-
-                        }
-
-                        index++
-                     }
-
-                     function isEmpty(dashboard){
-
-                        if(startDashboard[Number(coordinates[0])][Number(coordinates[1])] === false){
-
-                           return false;
+                           startDashboard[Number(coordinates[0])][Number(coordinates[1])] = marker                           
 
                         }else{
 
-                           return true;
+                           console.log("This place is already taken. Please choose another!");
+                        }                        
+
+                        if(freeSpace(startDashboard)){
+
+                           console.log("The game ended! Nobody wins :(") 
+
+                          return print(startDashboard);
+
+                        }
+
+                        if(hasWinner(startDashboard, marker)){
+
+                           console.log(`Player ${marker} wins!`);
+
+                           return print(startDashboard)
+
+                        }
+
+                        for(let line of startDashboard){
+
+                        console.log(line.join("\t"))
+
+                     } 
+                        
+                     }
+                     
+                     function freeSpace(dashboard){
+
+                        let res = dashboard.flat();
+
+                        let filtered = res.filter(x => x === false);
+
+                        if(filtered.length > 0){
+
+                           return false
+                        }
+
+                        return true
+
+                     }
+
+                     function hasWinner(dashboard, marker){
+
+                        for(let i = 0; i < dashboard.length; i++){
+                           
+                           if(dashboard[i][0] === marker &&
+                              dashboard[i][1] === marker &&
+                              dashboard[i][2] === marker 
+                           ){
+                              
+                              return true
+
+                           }else if(
+
+                              dashboard[0][i] === marker &&
+                              dashboard[1][i] === marker &&
+                              dashboard[2][i] === marker 
+
+                           ){
+
+                              return true;
+
+                           }else if(
+
+                              dashboard[0][0] === marker &&
+                              dashboard[1][1] === marker &&
+                              dashboard[2][2] === marker 
+
+                           ){
+
+                              return true;
+
+                           }else if(
+
+                              dashboard[0][2] === marker &&
+                              dashboard[1][1] === marker &&
+                              dashboard[2][0] === marker 
+
+                           ){
+
+                              return true;
+                              
+                           }
+
                         }
 
                      }
+
+                     function print(dashboard){
+
+                        for(let line of startDashboard){
+
+                        console.log(line.join("\t"))
+
+                     }
+
+                     }      
+                     
+
 }
-
-
 
