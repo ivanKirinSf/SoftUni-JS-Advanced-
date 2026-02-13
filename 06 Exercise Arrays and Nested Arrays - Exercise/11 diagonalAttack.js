15bd81d454624dc4a7bec174194962a8e4d41078
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-02-13 21:18:30 +0200

    Update 11 diagonalAttack.js

--- 06 Exercise Arrays and Nested Arrays - Exercise/11 diagonalAttack.js
+++ 06 Exercise Arrays and Nested Arrays - Exercise/11 diagonalAttack.js
@@ -1,30 +1,96 @@
 function diagonalAttack(data){
 
-    let list = data.split(", ")
+  let sumDig1 = 0;
+  let sumDig2 = 0;
 
-   if(equalsSums(data)){     
+  let info = data
+
+  let newArr = []
+
+ // let arr = data.split(", ")
+
+  for(let i = 0; i< info.length; i++){
+
+    arr = info[i].split(" ");
+
+    sumDig1 += Number(arr[i]);
+
+    sumDig2 += Number(arr[(arr.length-1) - i]);
+
+    newArr.push(arr)
+
+  }
+
+  //console.log(arr);
+
+  if(isEqual(sumDig1, sumDig2)){
+
     
 
-   }
+    //return console.log(newArr[0][0]) ;
 
-   function equalsSums(arr){
+    return setDiagonals(newArr);    
 
-    let sumDig1 = 0;
+  }
 
-    let sumDig2 = 0;
+  for(let row of newArr){
 
-   for(let i = 0; i < arr.length; i++){
+     //console.log(row.join("\t"))
+     console.log(row.join(" "))
 
-    sumDig1 += Number(arr[i][i]);
+    } 
 
-    sumDig2 += Number(arr[i][(arr.length-1)-i]);   
+  function isEqual(a, b){
 
-   }    
-   
-   console.log(sumDig1);
+    if(a === b){
 
-   console.log(sumDig2);
+      return true;
 
-   }
+    }else {
+
+      return false;
+
+    }
+
+  }
+
+  function setDiagonals(arr){
+
+    let lastArr = []
+
+    for(let i = 0; i < arr.length; i++){
+
+      let temp = arr[i];
+
+      for(let j = 0; j < temp.length; j++){
+
+        if(j !== i && j !== (temp.length-1) - i){
+
+          temp[j] = sumDig1;
+        }
+      }
+
+      lastArr.push(temp)
+    }
+
+    
+  for(let row of lastArr){
+
+     //console.log(row.join("\t"))
+
+     console.log(row.join(" "))
+
+  }
+
+  }                          
 
 }
+
+diagonalAttack(
+['5 3 12 3 1',
+'11 4 23 2 5',
+'101 12 3 21 10',
+'1 4 5 2 2',
+'5 22 33 11 1']
+
+)
