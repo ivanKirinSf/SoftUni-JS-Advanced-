75fd12afb582a205750ac7da87249742468789f4
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-03-06 19:48:13 +0200

    Create department.js

---
+++ 07 Objects and Composition - Lab/department.js
@@ -0,0 +1,11 @@
+let department = {
+
+    name: "Engeneering",
+    director: "Ted Thompson",
+    employeeCount: 25
+};
+
+let {name, employeeCount} = department;
+
+
+console.log(name, employeeCount)
