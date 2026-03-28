2bba9130368065716262bba523a6d92d3bc5bacb
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-03-28 15:25:06 +0200

    Update 03 cityTaxes.js

--- 07 Objects and Composition - Lab/03 cityTaxes.js
+++ 07 Objects and Composition - Lab/03 cityTaxes.js
@@ -1,30 +1,31 @@
 
-function cityTaxes(cityName, cityPopulation, cityThreasury){
+function cityTaxes(cityName, cityPopulation, cityTreasury){
 
     let city = {};
 
     city.name = cityName;
     city.population = cityPopulation;
-    city.treasury = cityThreasury;
-    city.taxRate = 10;
+    city.treasury = cityTreasury;
+    city.taxrate = 10; 
 
     city.collectTaxes = function(){
 
-        this.treasury += this.population * this.taxRate;
+        this.treasury += this.population * this.taxrate;
 
-    };
+    }
 
-    city.applyGrowth = function(percent){
+    city.applyGrowth = function(percentage){
 
-        this.population += this.population * (percent/100);
+        this.population += this.population*( percentage/100 );
 
     }
 
     city.applyRecession = function(percentage){
 
-        this.treasury -= this.treasury * percentage/100;
+        this.treasury -= this.treasury*( percentage/100 );
 
     }
 
-    return city
-} 
+    return city   
+
+}
