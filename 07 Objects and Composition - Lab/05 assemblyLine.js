e1b968a64b547a949b5eafc8e4db95ecb41e3f34
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-04-16 14:48:20 +0300

    Create 05 assemblyLine.js

---
+++ 07 Objects and Composition - Lab/05 assemblyLine.js
@@ -0,0 +1,24 @@
+function assemblyLine(data){
+
+    data.hasClima = () => {
+        
+    }
+
+
+
+}
+
+
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
