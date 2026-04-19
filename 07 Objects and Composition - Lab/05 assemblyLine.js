be47137e219826e888c531a51b772318e21b59a5
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-04-19 13:57:01 +0300

    Update 05 assemblyLine.js

--- 07 Objects and Composition - Lab/05 assemblyLine.js
+++ 07 Objects and Composition - Lab/05 assemblyLine.js
@@ -1,51 +1,25 @@
 function createAssemblyLine(){
 
-    return{
+hasClima(car){
 
-        hasClima(car){
-            car.temp = 21;
-            car.tempSettings = 21;
+    car.temp = 21;
+    car.tempSettings = 21;
 
-            car.adjustTemp = function (){
-                if(this.temp < tempSettings){
+    car.adjustTemp = () => {
 
-                    this.temp += 1;
-                    
-                }else if(this.temp > tempSettings){
+        if(this.temp < this.tempSettings){
+            this.temp += 1; 
+        }else if(this.temp > this.tempSettings){
+            this.temp -= 1;
+        };
 
-                    this.temp -= 1;
-                }
 
-            }
-        },
-
-        hasAudio(car){
-
-            car.currentTrack = null;
-
-            car.nowPlaying = function() {
-                if( this.currentTrack !== null ){
-
-                    console.log(`Now playing ${this.currentTrack.name} by ${this.currentTrack.artist}`);
-
-                }
-            }
-
-        }
-
-        hasParktronic(car) {
-            car.checkDistance = function (distance) {
-                if (distance < 0.1) {
-                    console.log("Beep! Beep! Beep!");
-                } else if (distance < 0.25) {
-                    console.log("Beep! Beep!");
-                } else if (distance < 0.5) {
-                    console.log("Beep!");
-                } else {
-                    console.log("");
-                }
-            };
-        }
     }
+
+
+
+
+}
+       
 
 }
