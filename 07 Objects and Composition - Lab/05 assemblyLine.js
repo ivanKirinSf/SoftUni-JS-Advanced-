caf070fb9cf856860c8dd48f6cd850cc611d0b3c
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-04-19 14:31:01 +0300

    Update 05 assemblyLine.js

--- 07 Objects and Composition - Lab/05 assemblyLine.js
+++ 07 Objects and Composition - Lab/05 assemblyLine.js
@@ -1,25 +1,40 @@
 function createAssemblyLine(){
 
+return{
+
+
 hasClima(car){
 
-    car.temp = 21;
-    car.tempSettings = 21;
+   car.temp = 21;
+   car.tempSettings = 21;
 
-    car.adjustTemp = () => {
+   car.adjustTemp = function (){
 
         if(this.temp < this.tempSettings){
-            this.temp += 1; 
+            this.temp += 1;
         }else if(this.temp > this.tempSettings){
-            this.temp -= 1;
-        };
+            this.temp -= 1;           
 
+        }
+   }
 
+},
+
+hasAudio(car){
+
+    car.currentTrack = null;
+
+    if(this.currentTrack !== null){
+
+        car.nowPlaying = function (){
+
+console.log(`Now playing ${this.currentTrack.name} by ${this.currentTrack.artist}`);
+
+        }
     }
 
-
-
+}
 
 }
-       
 
 }
