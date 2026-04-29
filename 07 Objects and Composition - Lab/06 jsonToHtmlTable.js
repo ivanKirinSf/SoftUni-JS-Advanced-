150c5f2bd25171262c50c9ac025c722b4f16f905
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-04-29 10:45:46 +0300

    Update 06 jsonToHtmlTable.js

--- 07 Objects and Composition - Lab/06 jsonToHtmlTable.js
+++ 07 Objects and Composition - Lab/06 jsonToHtmlTable.js
@@ -1,11 +1,7 @@
-function jsonToHtmlTable(json) {
-let arr = JSON.parse(json);
-let outputArr = ["<table>"];
-outputArr.push(makeKeyRow(arr));
-arr.forEach((obj)=> outputArr.push(makeValueRow(obj)));
-outputArr.push("</table>");
-  function makeKeyRow(arr)
-function makeValueRow(obj)
-function escapeHtml(value) 
-console.log(outputArr.join('\n'));
+function jsonToHtmlTable(json){
+
+    let output = JSON.parse(json);
+
+    console.table(output)
+    
 }
