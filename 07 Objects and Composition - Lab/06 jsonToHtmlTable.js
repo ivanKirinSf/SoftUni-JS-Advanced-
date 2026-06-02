da02094b1127b673a098371ca70c90e7ef80b084
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-06-02 21:51:48 +0300

    Update 06 jsonToHtmlTable.js

--- 07 Objects and Composition - Lab/06 jsonToHtmlTable.js
+++ 07 Objects and Composition - Lab/06 jsonToHtmlTable.js
@@ -2,40 +2,44 @@ function jsonToHtmlTable(json){
 
 let arr = JSON.parse(json);
 
-let outputArr = ['<table>'];
+let outputArr = [`<table>`];
 
 outputArr.push(makeKeyRow(arr));
-arr.forEach((obj) => outputArr.push(makeValuesRow(obj)));
+
+arr.forEach( (obj) => outputArr.push(makeValueRow(obj)));
 
 outputArr.push(`</table>`);
 
 function makeKeyRow(arr){
 
-    let keys = Object.keys(arr[0]);
+let keys = Object.keys(arr[0]);
 
-    let header = keys.map( k => `<th>${escapeHtml(k.trim())}</th>`).join("");
+let header = keys.map(k => `<th>${escapeHtml(k.trim())}</th>`).join("");
 
-    return `   <tr>${header}</tr>`
+return `   <tr>${header}</tr>`
 
 }
 
-function makeValuesRow(obj){
+function makeValueRow(obj){
 
     let values = Object.values(obj);
 
-    let txt = values.map( v => `<td>${escapeHtml(v)}</td>`).join("");
+    let row = values.map(v => `<td>${escapeHtml(v)}</td>`).join("");
 
-    return `   <tr>${txt}</tr>`
+    return `   <tr>${row}</tr>`
+
+    //console.log(values)    
 
 }
 
 function escapeHtml(value){
 
     return String(value)
-    .replace(/</g, `&lt`)
-    .replace(/>/g, `&gt`)
-    .replace(/&/g, `&amp`)
-    .replace(/"/g, `&quot`)
+    .replace(/&/g, `&amp;`)
+    .replace(/</g, `&lt;`)
+    .replace(/>/g, `&gt;`)
+    .replace(/"/g, `&quot;`)
+
 }
 
 console.log(outputArr.join(`\n`))
