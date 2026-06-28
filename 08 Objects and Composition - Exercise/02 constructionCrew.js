e6f4709df34e7be7587f5e48d10917af5e531de5
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-06-28 20:59:36 +0300

    Update 02 constructionCrew.js

--- 08 Objects and Composition - Exercise/02 constructionCrew.js
+++ 08 Objects and Composition - Exercise/02 constructionCrew.js
@@ -1,18 +1,15 @@
-const worker = {
-    weight: 80,
-  experience: 1,
-  levelOfHydrated: 0,
-  dizziness: true 
+function constructionWorker(worker){
+
+    if (worker.dizziness){
+
+        let waterAmount = worker.weight*worker.experience*0.1;
+
+        worker.levelOfHydrated += waterAmount;
+
+        worker.dizziness = false
+    }
+
+    return worker
+
 }
-
-if(worker.dizziness){
-
-    let water = worker.weight*worker.experience*0.1;
-
-    worker.levelOfHydrated += water;
-
-    worker.dizziness = false
-}
-
-console.log(worker)
 
