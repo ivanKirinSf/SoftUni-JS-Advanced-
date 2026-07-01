1c6286300cc0735cc9f94ad2cc9ca51cce1e9861
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-07-01 21:30:11 +0300

    Update 03 carFactory.js

--- 08 Objects and Composition - Exercise/03 carFactory.js
+++ 08 Objects and Composition - Exercise/03 carFactory.js
@@ -1,14 +1,68 @@
-function carFactory(order){
+function carFactory(car){
 
-    const car = {
+    const order = {};
 
-        model: undefined,
-        engine: null,
-        carriage: null,
-        wheels: null,
+    order.model = car.model;
+
+    let carPower = car.power;
+
+    if(carPower <= 90){
+
+       order.engine = {};
+
+       order.engine.power = 90;
+       order.engine.volume = 1800;
+
+    }else if(carPower <= 120){
+
+        order.engine = {};
+
+        order.engine.power = 120;
+        order.engine.volume = 2400;
+
+    }else if(carPower <= 200){
+
+        order.engine = {};
+
+        order.engine.power = 200;
+        order.engine.volume = 3500;
 
     }
 
-    
+    let carCarriage = car.carriage;
+
+    if(carCarriage = 'hatchback'){
+
+        order.carriage = {};
+
+        order.carriage.type = carCarriage;
+
+        order.carriage.color = car.color;
+
+    }else if(carCarriage = 'coupe'){
+
+        order.carriage = {};
+
+        order.carriage.type = carCarriage;
+
+        order.carriage.color = car.color;
+
+    }
+
+    let carWheels = car.wheelsize;
+
+    if( carWheels % 2 === 0 ){
+
+        let size = carWheels - 1;
+
+        let arr = [];
+
+        arr.push(...Array(4).fill(size));
+
+        order.wheels = arr;        
+
+    }
+
+    return console.log(JSON.stringify(order))
 
 }
