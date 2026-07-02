121d1a28164a1f5aac7f21885ad5a1fa35f51b81
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-07-02 21:14:00 +0300

    Update 05 lowestPriceInCities.js

--- 08 Objects and Composition - Exercise/05 lowestPriceInCities.js
+++ 08 Objects and Composition - Exercise/05 lowestPriceInCities.js
@@ -1,16 +1,78 @@
-function lowestPriceInCities(data){
+function carFactory(car){
 
-    let result = {};
+    const order = {};
 
-    for(let el of data){
+    order.model = car.model;
 
-        let temp = el.split(" | ");
+    let carPower = car.power;
 
-        let town = temp.shift();
-        let product = temp.shift();
-        let price = Number(temp.shift());
+    if(carPower <= 90){
 
-        console.log(price)
+       order.engine = {};
+
+       order.engine.power = 90;
+       order.engine.volume = 1800;
+
+    }else if(carPower <= 120){
+
+        order.engine = {};
+
+        order.engine.power = 120;
+        order.engine.volume = 2400;
+
+    }else {
+
+        order.engine = {};
+
+        order.engine.power = 200;
+        order.engine.volume = 3500;
+
     }
+
+    let carCarriage = car.carriage;
+
+    if(carCarriage === 'hatchback'){
+
+        order.carriage = {};
+
+        order.carriage.type = carCarriage;
+
+        order.carriage.color = car.color;
+
+    }else if(carCarriage === 'coupe'){
+
+        order.carriage = {};
+
+        order.carriage.type = carCarriage;
+
+        order.carriage.color = car.color;
+
+    }
+
+    let carWheels = car.wheelsize;
+
+    if( carWheels % 2 === 0 ){
+
+        let size = carWheels - 1;
+
+        let arr = [];
+
+        arr.push(...Array(4).fill(size));
+
+        order.wheels = arr;        
+
+    }else{
+
+        let size = carWheels
+
+        let arr = [];
+
+        arr.push(...Array(4).fill(size));
+
+        order.wheels = arr;          
+
+    }
+
+    return order
 
 }
