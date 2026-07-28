51be6f05fc5765723fd90f0d22a035bca59b3141
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-07-28 21:25:02 +0300

    Create rectangle.js

---
+++ 08 Objects and Composition - Exercise/rectangle.js
@@ -0,0 +1,15 @@
+function rectangle(width, height, color){
+
+    const rect = {
+
+        width: width,
+        height: height,
+        calcArea: function (width, height){
+
+            return width*height;
+            
+        }
+
+    };    
+
+}
