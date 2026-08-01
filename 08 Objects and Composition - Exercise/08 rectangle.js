11aa50832e9859b73805a502a4131d34f67a65b8
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-01 12:39:05 +0300

    Update 08 rectangle.js

--- 08 Objects and Composition - Exercise/08 rectangle.js
+++ 08 Objects and Composition - Exercise/08 rectangle.js
@@ -1,24 +1,28 @@
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
+
 function rectangle(width, height, color){
 
     let char = color[0].toUpperCase();
     let sliced = color.slice(1);
-    let newColor = char+sliced;
-
+    let newColor = char + sliced;
     //console.log(newColor)
 
-return{   
+    return {
 
-    width: Number(width),
-    height: Number(height),
-    color: newColor,
+         width: Number(width),
+         height: Number(height),
+         color: newColor,
 
-    calcArea(){
+         calcArea(){
 
-        return this.width*this.height;
+            return this.width*this.height; 
+
+         }
 
     }
 
 }
-
-}
-
