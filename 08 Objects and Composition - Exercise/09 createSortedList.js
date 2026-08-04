cc8b282464f89c939bb6eba590fb6b238bc5c3d7
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-04 17:15:47 +0300

    Update 09 createSortedList.js

--- 08 Objects and Composition - Exercise/09 createSortedList.js
+++ 08 Objects and Composition - Exercise/09 createSortedList.js
@@ -1,35 +1,48 @@
+let list = createSortedList();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1)); 
+list.remove(1);
+console.log(list.get(1));
+//console.log(list.size);
+
+
 function createSortedList(){
 
-let arr = [];
+    let arr = [];
 
-return{
+    return{
 
-add(el){
+          add(el){
 
-    arr.push(el);
-    arr.sort((a,b) => a-b);
+            arr.push(el);
+            arr.sort((a,b) => a-b);
 
-},
+          },
 
-remove(index){
+          remove(index){
 
-    if(index >= 0 && index < arr.length)
-    arr.splice(index, 1);
+            if(index >= 0 && index < arr.length){
 
-},
+             arr.splice(index,1);
 
-get(index){
+            }           
 
-    return arr[index];
+          },
 
-},
+          get(index){
 
-get size(){
+            return arr[index];
 
-    return arr.length;
+          },
 
-}
+          get size(){
 
-}
+             return arr.length
+
+          }
+
+    }
 
 }
