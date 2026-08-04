1e998d714e46ca63f94b29fbb8e3e98413ae9f93
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-04 17:42:03 +0300

    Update 10 heroes.js

--- 08 Objects and Composition - Exercise/10 heroes.js
+++ 08 Objects and Composition - Exercise/10 heroes.js
@@ -1,31 +1,32 @@
 function solve(){
 
-       return{
 
-         mage(name){
+    return {
 
-            health: 100;
-            mana: 100,
+         mage(fighterName){
 
+            name,
+            health = 100,
+            mana = 100,
             cast(spell){
-
-               this.mana -= 1;
-               console.log(`${name} cast ${spell}`)
+                this.mana -= 1;
+                console.log(`${this.name} cast ${spell}`)
             }
          },
 
-         fighter(name){
+         fighter(fighterName){
 
-            health: 100,
-            stamina: 100,
-
+            name,
+            health = 100,
+            stamina = 100,
             fight(){
-               stamina -= 1;
-               console.log(`${name} slashes at the foe!`)
-            }
+                
+                stamina -=1;
+                console.log(`${this.name} slashes at the foe!`)
 
+            }
          }
 
-       }
 
+    }
 }
