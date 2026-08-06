572c4127e4ea32685e7c2c6fec39dbf174dafe33
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-06 16:54:22 +0300

    Update 10 heroes.js

--- 08 Objects and Composition - Exercise/10 heroes.js
+++ 08 Objects and Composition - Exercise/10 heroes.js
@@ -1,39 +1,54 @@
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball")
+scorcher.cast("thunder")
+
+scorcher.cast("light")
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight()
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
+
 function solve(){
 
-return {
+   return{
 
-    mage(mageName){
+      mage(mageName){
 
-      return{
+         return{
 
-        name: mageName,
-        health: 100,
-        mana: 100,
+               name: mageName,
+               health: 100,
+               mana: 100,
 
-        cast(spell){
+               cast(spell){
 
-          this.mana -= 1;
-          console.log(`${this.name} cast ${spell}`)
-        }
+                    this.mana -= 1;
+                    console.log(`${this.name} cast ${spell}`);
+
+               }
+
+         }
+
+      },
+
+      fighter(fighterName){
+
+         return {
+
+         name: fighterName,
+         health: 100,
+         stamina: 100,
+
+         fight(){
+
+            this.stamina -= 1;
+            console.log(`${this.name} slashes at the foe!`);
+
+         }
       }
-    },
-
-    fighter(fighterName){
-
-      return{
-       
-        name: fighterName,
-        health: 100,
-        stamina: 100,
-
-        fight(){
-
-          this.stamina -= 1;
-          console.log(`${this.name} slashes at the foe!`);
-        }
       }
-    }
+   }
 }
-}
-
-//check again!!!
