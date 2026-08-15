b89f3d9eb1f28b31bf001da4e799a0e567e1a1f1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-15 12:54:31 +0300

    Update 11 janNotation.js

--- 08 Objects and Composition - Exercise/11 janNotation.js
+++ 08 Objects and Composition - Exercise/11 janNotation.js
@@ -1,107 +1,26 @@
 function janNotation(data){
 
-let arr = data;
-let numA = 0;
-let numB = 0;
-let res = 0;
-let isValid = false;
+  let arr = [];
 
-while(arr.length > 1){
+obj = {
 
-  i=0;
+  "+": (a,b) => a+b,
+  "-": (a,b) => a-b,
+  "*": (a,b) => a*b,
+  "/": (a,b) => a*b,
 
-  for(let i = 0; i < arr.length; i++){
+}
 
-    let temp = arr[i];
+  for(let el of data){
 
-    if(temp === "+"){
+    let type = typeof(el)
 
-      numA = Number(arr[i-2]);
-      numB = Number(arr[i-1]);
+    if(type === "number"){
 
-      res = numA + numB;
+      arr.push(el)
 
-      if(res){
-
-        arr.splice(i-2,3);
-        arr.splice(i-2,0,res);
-
-      }
-    }else if(temp === "-"){
-
-      numA = Number(arr[i-2]);
-      numB = Number(arr[i-1]);
-
-      res = numA - numB;
-
-      if(res){
-
-        arr.splice(i-2,3);
-        arr.splice(i-2,0,res);
-
-      }
-
-    }else if(temp === "*"){
-
-      numA = Number(arr[i-2]);
-      numB = Number(arr[i-1]);
-
-      res = numA * numB;
-
-      if(res){
-
-        arr.splice(i-2,3);
-        arr.splice(i-2,0,res);
-
-      }
-
-    }else if(temp === "/"){
-
-      numA = Number(arr[i-2]);
-      numB = Number(arr[i-1]);
-
-      res = numA / numB;
-
-      if(res){
-
-        arr.splice(i-2,3);
-        arr.splice(i-2,0,res);
-
-      }
     }
-  }
-
-  for(let el of arr){
-  let temp = el;
-
-  if(el === "+" || el ==="-" || el ==="*" | el === "/"){
-
-    isValid = true
 
   }
-}
-if(isValid === false){
-  
-  break;
-}
 
 }
-
-if(isValid === false){
-
-  //console.log(arr.join(""))
-  console.log("Error: not enough operands!")
-  
-}else{
-
-console.log(arr.join(""))
-}
-}
-
-janNotation(
-  [
-  3,
- 4,
- '+'
-]
-)
