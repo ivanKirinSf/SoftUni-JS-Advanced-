34989557a5983dd7ef8e9bd9bc411bdcd58d3447
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-16 11:56:32 +0300

    Update 11 janNotation.js

--- 08 Objects and Composition - Exercise/11 janNotation.js
+++ 08 Objects and Composition - Exercise/11 janNotation.js
@@ -1,6 +1,10 @@
 function janNotation(data){
 
   let arr = [];
+  let operator = "false";
+  
+  let result = 0;
+
 
 obj = {
 
@@ -11,16 +15,48 @@ obj = {
 
 }
 
-  for(let el of data){
+let index = 0;
+  while(data.length > 0){ 
+    
+    let el = data[index];
 
-    let type = typeof(el)
+    if(Number(el)){
 
-    if(type === "number"){
+      arr.push(el);
+      data.splice(index, 1)
+    }
+           
+      //let a = Number(data[index-1]);      
+      //let b = Number(data[index-2]);
+      //arr.push(a);
+      //arr.push(b);
+      //data.splice(index-1, 1);
+      //data.splice(index-2, 1);
 
-      arr.push(el)
+    
+    if(el === "+" || el === "-" || el === "*" || el === "/"){
+
+      operator = el;
+      data.splice(index, 1);    
+      
+
+      if(a, b){
+
+    result = obj[operator](a,b);
+    arr.push(result);
+
+     }
 
     }
 
-  }
+    index ++
+
+  }  
+  
+  console.log(result)
+  console.log(data)
+  console.log(arr)
+
+   
 
 }
