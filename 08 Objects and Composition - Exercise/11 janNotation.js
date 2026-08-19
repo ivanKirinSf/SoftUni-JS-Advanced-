931b67f6506422aefb79310cb3942062580ae311
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-19 18:10:40 +0300

    Update 11 janNotation.js

--- 08 Objects and Composition - Exercise/11 janNotation.js
+++ 08 Objects and Composition - Exercise/11 janNotation.js
@@ -2,41 +2,52 @@ function janNotation(data){
 
   let arr = [];
 
-  let obj = {
+  let operators = {
 
-    "+": (a,b) => a+b,
-    "-": (a,b) => a-b,
-    "*": (a,b) => a*b,
-    "/": (a,b) => a/b,
+    "+": (a, b) => a+b,
+    "-": (a, b) => a-b,
+    "*": (a, b) => a*b,
+    "/": (a, b) => a/b,
 
   }
 
   for(let el of data){
 
-let type = typeof(el);
+    let type = typeof el;
 
-if(type === "number"){
+    if(type === "number"){
 
-  arr.push(el);
+      arr.push(el);
 
-}else if(arr.length < 1){
+    }else{
 
-  console.log("Error: not enough operands!");
+      if(arr.length < 2){
 
-}
+        console.log("Error: not enough operands!");
+        return;
 
-  let num1 = arr.pop();
-  let num2 = arr.pop();
-  let res = obj[el](num2, num1)
+      }
+
+      let num1 = arr.pop();
+      let num2 = arr.pop();
+      let res = operators[el](num2, num1);
+      arr.push(res);
+
+    }
 
   }
 
+
   if(arr.length === 1){
+
     console.log(arr[0]);
+
+
   }else{
 
-    console.log("Error: too many operands!")
+    console.log("Error: too many operands!");
 
-  }  
+  }
+
 
 }
