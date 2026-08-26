4c6d7388260e762e7eb4f7f99a5a55ca48fa883f
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-26 17:35:23 +0300

    Create 03 sumNumbers.js

---
+++ 09 DOM Introduction/03 sumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+    
+    const input1 = document.getElementById('num1');
+    const input2 = document.getElementById('num2');
+    document.getElementById('sum').value = Number(input1.value) + Number(input2.value);
+
+}
