f4a75f1e4fd23bae4f640c2edad53e03645308ae
Ivan Kirin <arch.ivan.kirin@gmail.com>
2026-08-27 13:50:16 +0300

    Create 01 editElement.js

---
+++ 09 DOM Introduction/01 editElement.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById(`e1`);
+
+console.log(document)
+
+
+
+function editElement(ref, match, replacer) {
+   
+const content = ref.textContent;
+const matcher = new RegExp(match, 'g');
+const edited = content.replace(matcher, replacer);
+ref.textContent = edited;
+
+}
